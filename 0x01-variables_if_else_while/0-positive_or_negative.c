e538073352da6d933f58fcd9ec9ca0e213eb3a2e
Egar King David <egarkingdavid007@gmail.com>
2022-04-08 04:52:40 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,12 @@
  */
 int main(void)
 {
-
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-        	printf("%i is positive\n", n);
+		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
 	else
