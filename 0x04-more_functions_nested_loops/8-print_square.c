626d71d73f8d3e8881c7ff0fd0baa978d88cdb05
Egar King David <egarkingdavid007@gmail.com>
2022-04-14 04:51:42 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_square(int n)
+{
+
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+
