585e2014de1e528d87a6a4280d5792685a2c034e
Egar King David <egarkingdavid007@gmail.com>
2022-05-09 16:58:50 -0700

    merge

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 main.h
 
 
@@ -412,4 +413,34 @@ int main(int argc, char *argv[])
 	else
 		multiply(n1, n2);
 	return (0);
+=======
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
+	}
+	mul = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
+>>>>>>> 841296f9b1fa23d4fb8e6692694ecf1410da5179
 }
